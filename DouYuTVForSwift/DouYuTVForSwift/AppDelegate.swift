13287b8690ce3484761f37ffa7199b49984b7c67
yuhao-ios <1481569310@qq.com>
2017-04-14 16:36:32 +0800

    初始化

--- DouYuTVForSwift/DouYuTVForSwift/AppDelegate.swift
+++ DouYuTVForSwift/DouYuTVForSwift/AppDelegate.swift
@@ -15,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+       
+        //设置tabBar的选中颜色
+        UITabBar.appearance().tintColor = UIColor.orange
+        
+
+        
+        
+        
+        
+        
         return true
     }
 
---
+++ DouYuTVForSwift/DouYuTVForSwift/Main/MainTabBarController.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabBarController.swift
+//  DouYuTVForSwift
+//
+//  Created by t4 on 17/4/14.
+//  Copyright © 2017年 t4. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        addChildVC(storyName: "Home")
+        addChildVC(storyName: "Live")
+        addChildVC(storyName: "Concern")
+        addChildVC(storyName: "My")
+        
+        
+    }
+    
+    
+    
+    
+    
+    //MARK: - 添加子控制器
+    fileprivate func addChildVC(storyName:String){
+    
+    
+        //1.加载storyBoard文件
+        
+        let childVC = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!;
+        
+        
+        //2.将控制器作为子控制器
+        
+        addChildViewController(childVC)
+
+
+    }
+   
+}
